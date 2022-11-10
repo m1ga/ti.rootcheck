7bb1bbc514ed168ce3053357bea82b9ae7ad7cc8
m1ga <migamiga@web.de>
2022-11-10 18:28:59 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Uses https://github.com/scottyab/rootbeer
+
+
+```js
+var rootCheck = require('ti.rootcheck');
+console.log(rootCheck.root);
+```
---
+++ example/app.js
@@ -0,0 +1,7 @@
+var win = Ti.UI.createWindow();
+var label = Ti.UI.createLabel();
+win.add(label);
+win.open();
+
+var rootCheck = require('ti.rootcheck');
+label.text = "Is rooted: " + rootCheck.root;
